743470932919a088592c712d68f9c8336b95a190
matejmich <matejmich@seznam.cz>
2023-10-14 11:34:42 +0200

    UI done, display does what it is supposed to do Buttons dont work

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onload = function(){ 
+let displayValue = '0';
+function updateDisplay() {
+    const display = document.getElementById('display');
+    display.innerText = displayValue;
+    if(displayValue.length > 9) {
+        display.innerText = displayValue.substring(0, 9);
+    }
+}
+  
+updateDisplay();
+
+
+
+
+
+
+
+}
